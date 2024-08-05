fe71bcc1791b16adc9ddfafa9235ec4cd198cfd4
M-Tijani <mahditijani1@gmail.com>
2024-08-05 22:36:56 +0100

    Starting Project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Signin from "@/components/Signin";
+
+export default function Home() {
+  return (
+    <main className="w-full h-full min-h-screen flex items-center justify-center bg-background_main">
+      <Signin />
+    </main>
+  );
+}
